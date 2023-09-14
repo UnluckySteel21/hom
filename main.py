e22664749c5035b5ba297e20a909f72248835843
Davis_R <davisromanovskis5@gmail.com>
2023-09-14 20:14:31 +0300

    v0.1 ar visām izmaiņām točna

--- main.py
+++ main.py
@@ -5,31 +5,18 @@ class car():
     def __init__(self):
         self.brand = "Mašīnas marka"
         self.model = "Mašīnas modelis"
-        self.PhoneNumber = "Telefona numurs"
-        self.WinNumber = "Win numurs"
-        self.Date = "Datums"
+        self.phoneNumber = "Telefona numurs"
+        self.winNumber = "Win numurs"
+        self.date = "Datums"
         self.description = "Apraksts"
         self.id = "id"
+        self.licensePlate = ""
     
-def changePar():
-    entries.append(b)
+    def changePar():
+        pass
 
 def Onload():
-    global entries, b
-    entries = []
     root = tk.Tk()
-    height = 1
-    width = 7
     displayFrame = tk.Frame(root, border=10)
     displayFrame.pack()
-    for i in range(height):
-        for j in range(width):
-            b = tk.Entry(displayFrame)
-            b.grid(row=i, column=j)
-    button = tk.Button(displayFrame, function=changePar())
-    button.grid(column= j+1, row=i+1)
-    
     root.mainloop()
-Onload()
-for entry in entries:
-    print(entry.get())
