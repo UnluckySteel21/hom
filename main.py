a52e9d81329e2ca194fa216ccdb6847204294fd7
Davis_R <davisromanovskis5@gmail.com>
2023-09-15 21:36:09 +0300

    v0.2 Uzsākta iedavrindas pievienošana. Neliela optimizācija mašīnu klasei

--- main.py
+++ main.py
@@ -1,22 +1,47 @@
+#importing things i need for the project
 import tkinter as tk
 import random
 
+
+#car class that is somewhat a default spreadsheet
 class car():
-    def __init__(self):
-        self.brand = "Mašīnas marka"
-        self.model = "Mašīnas modelis"
-        self.phoneNumber = "Telefona numurs"
-        self.winNumber = "Win numurs"
-        self.date = "Datums"
-        self.description = "Apraksts"
-        self.id = "id"
-        self.licensePlate = ""
+    def __init__(self, brand, model, phoneNumber, winNumber, date, description, identification, licensePlate):
+        self.brand = brand
+        self.model = model
+        self.phoneNumber = phoneNumber
+        self.winNumber = winNumber
+        self.date = date
+        self.description = description
+        self.identification = identification
+        self.licensePlate = licensePlate
     
     def changePar():
         pass
 
+#the main function of the programm that displays the start of the programm
 def Onload():
     root = tk.Tk()
-    displayFrame = tk.Frame(root, border=10)
-    displayFrame.pack()
+
+    #the input frame. Deals with new input + the description about what to write where
+    inputFrame = tk.Frame(root, border=10)
+    inputFrame.pack()
+    identificationLabel = tk.Label(inputFrame, text="Identifikācija")
+    identificationLabel.grid(column=1, row=1)
+    dateLabel = tk.Label(inputFrame, text="Datums un laiks")
+    dateLabel.grid(column=2, row=1)
+    brandLabel = tk.Label(inputFrame, text="Marka")
+    brandLabel.grid(column=3, row=1)
+    modelLabel = tk.Label(inputFrame, text="Modelis")
+    modelLabel.grid(column=4, row=1)
+    phoneNumberLabel = tk.Label(inputFrame, text="Telefona numurs")
+    phoneNumberLabel.grid(column=5, row=1)
+    winNumberLabel = tk.Label(inputFrame, text="Mašīnas win numurs")
+    winNumberLabel.grid(column=6, row=1)
+    licensePlateLabel = tk.Label(inputFrame, text="Mašīnas numurzīme")
+    licensePlateLabel.grid(column=7, row=1)
+    descriptionLabel = tk.Label(inputFrame, text="Apraksts")
+    descriptionLabel.grid(column=8, row=1)
+    
     root.mainloop()
+
+Onload()
